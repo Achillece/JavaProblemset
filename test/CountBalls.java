977ac1fbad9d0988fecc9f1815db19aa01185798
Achillece <51154010+achillece@users.noreply.github.com>
2025-02-13 23:45:48 +0800

    countBalls

---
+++ test/CountBalls.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=1742 lang=java
+ *
+ * [1742] Maximum Number of Balls in a Box
+ */
+
+// @lc code=start
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int countBalls(int lowLimit, int highLimit) {
+        Map<Integer, Integer> count = new HashMap<Integer, Integer>();
+        int res = 0;
+        for (int i = lowLimit; i <= highLimit; i++) {
+            int box = 0, x = i;
+            while (x != 0) {
+                box += x % 10;
+                x /= 10;
+            }
+            count.put(box, count.getOrDefault(box, 0) + 1);
+            res = Math.max(res, count.get(box));
+        }
+        return res;
+    }
+}
+// @lc code=end
+
